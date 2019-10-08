cc8fe71cbb2d2aeea88ecac022c5413c50cf33f8
imadpranjga <54896692+imadpranjga@users.noreply.github.com>
2019-10-08 11:01:52 -0400

    hfhf

--- Imad.js
+++ Imad.js
@@ -1,1 +1,8 @@
-const thirdAngle = (angle1, angle2) => (angle1 && angle2 && (angle3 = 180 - (angle1 + angle2)) > 0) ? angle3 : 'Invalid - Sum of all angles of a triangle should be 180 degrees';
+function thirdangle() {
+
+let angle1 = prompt("give 1st angle");
+let angle2 = prompt("give 2nd angle");
+let angle3 = 180 - angle1-angle2 ;
+alert ("The 3rd angle is " + angle3);
+
+}
