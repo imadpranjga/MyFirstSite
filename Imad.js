f501a9ca0da9b1dcd550dd2f20cf4793002026a8
imadpranjga <54896692+imadpranjga@users.noreply.github.com>
2019-10-10 10:52:59 -0400

    ete

--- Imad.js
+++ Imad.js
@@ -6,3 +6,14 @@ let angle3 = 180 - angle1-angle2 ;
 alert ("The 3rd angle is " + angle3);
 
 }
+
+function centuries() {
+
+let year = prompt (enter year);
+let cent = year/100 + 1 ;
+alert ("the century is" + cent);
+Math.round(0.5);    // returns 5
+Math.round(0.4);    // returns 4
+
+
+}
