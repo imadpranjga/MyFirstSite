c4dcda7cd6a78d7ea650787cb9f91a379a3c3462
imadpranjga <54896692+imadpranjga@users.noreply.github.com>
2019-10-22 10:20:17 -0400

    yeah

--- Imad.js
+++ Imad.js
@@ -12,8 +12,26 @@ function centuries() {
 let year = prompt (enter year);
 let cent = year/100 + 1 ;
 alert ("the century is" + cent);
-Math.round(0.5);    // returns 5
-Math.round(0.4);    // returns 4
+Math.round(0.5);    // returns 1
+Math.round(0.4);    // returns 0
+}
+const h1 = document.querySelector('h1');
+const paragraph = document.querySelector
 
+function centuryFromYear(year)
+ {
+  var x = Math.floor(year / 100); if (year % 100 === 0) return x; else return x + 1;
+   }
 
+   <p id="demo"></p>
+
+<script> 
+var x = 100;
+var y = 4;
+var z = x % x;
+document.getElementById("demo").innerHTML = z;
+</script>
+var i;
+for  (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
 }
