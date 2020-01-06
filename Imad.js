9f3266d10c0328f0a9db1ce19168ee438cedd7ed
imadpranjga <54896692+imadpranjga@users.noreply.github.com>
2020-01-06 10:55:51 -0500

    1
    asa

--- Imad.js
+++ Imad.js
@@ -45,7 +45,7 @@ function WordLength(){
 function Encrypt(){
     var a = prompt("Enter the keyword")
     const b = a.toLowerCase()
-    var c = b.replace(/a/g, "1").replace(/b/g, "2").replace(/c/g, "3").replace(/d/g, "4").replace(/e/g, "5").replace(/f/g, "6").replace(/g/g, "7").replace(/h/g, "8").replace(/i/g, "9").replace(/j/g, "(").replace(/k/g, ")").replace(/l/g, "-").replace(/m/g, "_").replace(/n/g, "+").replace(/o/g, "=").replace(/p/g, "{").replace(/q/g, "}").replace(/r/g, "[").replace(/s/g, "]").replace(/t/g, ";").replace(/u/g, ":").replace(/v/g, "'").replace(/w/g, "/").replace(/x/g, "?").replace(/y/g, ".").replace(/z/g, "^")
+    var c = b.replace(/a/g, "@").replace(/b/g, "").replace(/c/g, "#").replace(/d/g, "1").replace(/e/g, "%").replace(/f/g, "2").replace(/g/g, "&").replace(/h/g, ";").replace(/i/g, "4").replace(/j/g, "'").replace(/k/g, "5").replace(/l/g, "-").replace(/m/g, "6").replace(/n/g, "3").replace(/o/g, ":").replace(/p/g, "<").replace(/q/g, ">").replace(/r/g, ",").replace(/s/g, "0").replace(/t/g, "9").replace(/u/g, "`").replace(/v/g, "8").replace(/w/g, "=").replace(/x/g, "7").replace(/y/g, "_").replace(/z/g, "~")
      if (/[0-9]/.test(a)){
         alert("null")
       }else{
@@ -55,7 +55,7 @@ function Encrypt(){
   
   function Decrypt(){
     var a = prompt("Enter the keyword")
-    var c = a.replace(/!/g, "z").replace(/@/g, "y").replace(/#/g, "x").replace(/1/g, "w").replace(/%/g, "v").replace(/2/g, "u").replace(/&/g, "t").replace(/;/g, "s").replace(/4/g, "r").replace(/'/g, "q").replace(/5/g, "p").replace(/-/g, "o").replace(/6/g, "n").replace(/3/g, "m").replace(/:/g, "l").replace(/</g, "k").replace(/>/g, "j").replace(/,/g, "i").replace(/0/g, "h").replace(/9/g, "g").replace(/`/g, "f").replace(/8/g, "e").replace(/=/g, "d").replace(/7/g, "c").replace(/_/g, "b").replace(/~/g, "a")
+    var c = a.replace(/@/g, "a").replace(/!/g, "b").replace(/#/g, "c").replace(/1/g, "d").replace(/%/g, "e").replace(/2/g, "f").replace(/&/g, "g").replace(/;/g, "h").replace(/4/g, "i").replace(/'/g, "j").replace(/5/g, "k").replace(/-/g, "l").replace(/6/g, "m").replace(/3/g, "n").replace(/:/g, "o").replace(/</g, "p").replace(/>/g, "q").replace(/,/g, "r").replace(/0/g, "s").replace(/9/g, "t").replace(/`/g, "u").replace(/8/g, "v").replace(/=/g, "w").replace(/7/g, "x").replace(/_/g, "y").replace(/~/g, "z")
     
         alert(c)
       
